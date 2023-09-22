79f4a920e11961b779db785bfdfe52d9612bd7dc
limonhossen <limon15-2461@diu.edu.bd>
2023-09-22 22:35:15 +0600

    finish the project with react route

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 import {
   createBrowserRouter,
@@ -9,21 +8,30 @@ import {
 import Root from './components/Root/Root';
 import Home from './components/Home/Home';
 import AppliedJobs from './components/AppliedJobs/AppliedJobs';
-import ErrorPage from './components/EroorPage/ErrorPage';
+
+import JObDetails from './components/JobDetails/JobDetails';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    errorElement: <ErrorPage></ErrorPage>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
+      }, 
+      {
+        path: '/applied', 
+        element: <AppliedJobs></AppliedJobs>,
+        loader: () => fetch('/jobs.json') // warning: only load the data you need. do not load all the data
       },
       {
-        path: '/applied',
-        element: <AppliedJobs></AppliedJobs>
+        path: '/job/:id',
+        element:<JObDetails></JObDetails>, 
+        loader: () => fetch('/jobs.json') // do not load all data. load only what you need
       }
     ]
   },
@@ -31,6 +39,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
